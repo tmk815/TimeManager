6268691b73a63b38fdd7814df7e8158d93153f9c
tomoki <monndenn15@gmail.com>
2017-04-28 14:12:46 +0900

    Create DateDialog

---
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/DatePick.java
@@ -0,0 +1,32 @@
+package com.example.tomoki.timemanager;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+/**
+ * Created by tomoki on 2017/04/28.
+ */
+
+public class DatePick extends DialogFragment implements DatePickerDialog.OnDateSetListener{
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        final Calendar calendar=Calendar.getInstance();
+        int year= calendar.get(Calendar.YEAR);
+        int month=calendar.get(Calendar.MONTH)+1;
+        int day=calendar.get(Calendar.DAY_OF_MONTH);
+
+        return new DatePickerDialog(getActivity(),(MainActivity)getActivity(),year,month,day);
+
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+    }
+}
--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/MainActivity.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.tomoki.timemanager;
 
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,DatePickerDialog.OnDateSetListener {
 
+    private TextView dateText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dateText=(TextView)findViewById(R.id.dateText);
+        Button dateButton=(Button)findViewById(R.id.date);
+        dateButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view,int year,int monthOfYear,int dayOfMonth){
+        dateText.setText(String.valueOf(year)+"/"+ String.valueOf(monthOfYear));
+    }
+
+    public void showDatePiclerDialog(View v){
+        DialogFragment newFlagment=new DatePick();
+        newFlagment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(R.id.date==v.getId()){
+
+        }
     }
 }
