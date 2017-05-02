65734ecb113784fc1b9fa6c1489a02a42648f920
tomoki <monndenn15@gmail.com>
2017-05-02 15:06:56 +0900

    Write SQL

--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/DatabaseHelper.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/DatabaseHelper.java
@@ -5,18 +5,24 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper{
-    
+
+    static final String DB_NAME="timemanager.db";
+    static final int DB_VERSION=1;
+    static final String CREATE_TABLE="create table timedb ( _id integer primary key autoincrement, time integer not null );";
+    static final String DROP_TABLE="drop table timedb;";
+
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(DROP_TABLE);
+        onCreate(db);
     }
 }
