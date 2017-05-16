960db2a3cf7c474a95fefbf6b7804a389c6559b8
tomoki <monndenn15@gmail.com>
2017-05-16 14:38:04 +0900

    Add menu item and menu bar

--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/MainActivity.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -357,6 +359,13 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         text=1;
         DialogFragment newFragment = new TimePick();
         newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
 }
