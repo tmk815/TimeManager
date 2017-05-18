89aa46a2712fab8919cf9d90636806d73901b7eb
tomoki <monndenn15@gmail.com>
2017-05-19 00:36:22 +0900

    Change extends Activity to AppCompatActivity

--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/SettingsActivity.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/SettingsActivity.java
@@ -3,16 +3,18 @@ package com.example.tomoki.timemanager;
 import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 /**
  * Created by tomoki on 2017/05/17.
  */
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Setting");
         getWindow().addFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         FragmentTransaction fragmentTransaction =
