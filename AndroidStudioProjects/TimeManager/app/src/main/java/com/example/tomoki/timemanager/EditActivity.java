6368559ae3bed56de7b14c0b13e508fcbfd12fb0
tomoki <monndenn15@gmail.com>
2017-05-24 21:27:46 +0900

    Create EditActivity's File

---
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/EditActivity.java
@@ -0,0 +1,17 @@
+package com.example.tomoki.timemanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tomoki on 2017/05/24.
+ */
+
+public class EditActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
