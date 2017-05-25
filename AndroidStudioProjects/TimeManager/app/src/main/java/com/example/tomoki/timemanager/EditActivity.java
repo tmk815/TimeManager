f67b02a03109b471e3e2649fea8bb33d1f93ca22
tomoki <monndenn15@gmail.com>
2017-05-25 09:17:41 +0900

    Add BackButton

--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/EditActivity.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/EditActivity.java
@@ -1,7 +1,10 @@
 package com.example.tomoki.timemanager;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by tomoki on 2017/05/24.
@@ -11,7 +14,18 @@ public class EditActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_edit);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
