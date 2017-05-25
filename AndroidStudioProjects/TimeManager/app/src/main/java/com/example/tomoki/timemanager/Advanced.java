351ce9aaf1044d8544c16e8b1f755cce9c9bbcd6
tomoki <monndenn15@gmail.com>
2017-05-25 10:55:42 +0900

    Create advanced.xml and change Advanced.java

--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/Advanced.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/Advanced.java
@@ -1,5 +1,7 @@
 package com.example.tomoki.timemanager;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +9,12 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class Advanced extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.advanced);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
 }
