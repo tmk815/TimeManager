368a01e64f308c49c02039550542ed72cfd3824d
tomoki <monndenn15@gmail.com>
2017-06-06 15:23:14 +0900

    Create EditTimePick.java

---
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/EditTimePick.java
@@ -0,0 +1,27 @@
+package com.example.tomoki.timemanager;
+
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class EditTimePick extends DialogFragment implements TimePickerDialog.OnTimeSetListener{
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(), (EditActivity)getActivity(), hour, minute, true);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
