4a27f1feacc2b985ea2996eafe6a324b8f006bec
tomoki <monndenn15@gmail.com>
2017-06-06 15:27:35 +0900

    Add Edit Processing

--- AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/EditActivity.java
+++ AndroidStudioProjects/TimeManager/app/src/main/java/com/example/tomoki/timemanager/EditActivity.java
@@ -1,23 +1,34 @@
 package com.example.tomoki.timemanager;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 /**
  * Created by tomoki on 2017/05/24.
  */
 
-public class EditActivity extends AppCompatActivity{
+public class EditActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
     private Cursor cursor=null;
     private DatabaseHelper databaseHelper;
     private SQLiteDatabase timedb;
     private String id;
+    private int text=0;
+    private TextView editDateText,editsTime,editeTime,editPlace;
+    private NumberPicker editBreakTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,13 @@ public class EditActivity extends AppCompatActivity{
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         setTitle("編集");
+        editDateText = (TextView) findViewById(R.id.editDateText);
+        editsTime = (TextView) findViewById(R.id.editStartTimeText);
+        editeTime = (TextView) findViewById(R.id.editEndTimeText);
+        editPlace = (TextView) findViewById(R.id.editPlace);
+        editBreakTime = (NumberPicker) findViewById(R.id.editBreakTime);
+        editBreakTime.setMinValue(0);
+        editBreakTime.setMaxValue(150);
 
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
@@ -33,6 +51,41 @@ public class EditActivity extends AppCompatActivity{
         databaseHelper = new DatabaseHelper(getApplicationContext());
         timedb = databaseHelper.getWritableDatabase();
         cursor = timedb.query("timedb", null, "_id = "+id, null, null, null, null);
+    }
+
+    //日付修正
+    public void editDatePickerDialog(View v) {
+        DialogFragment newFragment = new EditDatePick();
+        newFragment.show(getSupportFragmentManager(), "editDatePicker");
+    }
+    //開始時刻修正
+    public void editStartTimePickerDialog(View v) {
+        text = 0;
+        DialogFragment newFragment = new EditTimePick();
+        newFragment.show(getSupportFragmentManager(), "editTimePicker");
+    }
+
+    //終了時刻修正
+    public void editEndTimePickerDialog(View v) {
+        text = 1;
+        DialogFragment newFragment = new EditTimePick();
+        newFragment.show(getSupportFragmentManager(), "editTimePicker");
+    }
+
+    //修正後の時刻をTextViewに表示
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        if (text == 0) {
+            editsTime.setText(String.format("%02d:%02d", hourOfDay, minute));
+        } else {
+            editeTime.setText(String.format("%02d:%02d", hourOfDay, minute));
+        }
+    }
+
+    //取得した年月日をTextViewに表示
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        editDateText.setText(String.format("%d-%02d-%02d", year, monthOfYear + 1, dayOfMonth));
     }
 
     @Override
